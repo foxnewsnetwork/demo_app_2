9addb1327d409414454d9f9c676ae40d0d6a9124
Thomas Chen <foxnewsnetwork@gmail.com>
2011-09-26 10:30:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calhack2::Application.config.secret_token = '595c874f17265fd55c155bf500fdb7923abacb1be696c48d933faf9d2cce1caf2566ae989a2333010b74f7f46abca2185e13c21913100cc51d592ff40f3914e1'
